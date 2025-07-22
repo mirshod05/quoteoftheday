4cdcea6a0eebaf817cb1bc50a61db82f712d7f83
mirshod05 <mirshodsobirov05@gmail.com>
2025-07-22 12:53:35 -0400

    Create qotd.py

---
+++ qotd.py
@@ -0,0 +1,26 @@
+import requests
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    await update.message.reply_text(
+        "Hi and welcome to the Quote of the day bot. Type /quote for a random quote."
+    )
+def get_quote() -> str:
+    response = requests.get('https://zenquotes.io/api/random')
+    data = response.json()[0]
+    quote = data['q'] + ' - ' + data['a']
+    return quote
+
+async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    quote_text = get_quote()
+    await update.message.reply_text(quote_text)
+
+BOT_TOKEN = "7805511089:AAEfC2E8sW_l-hj5gkt7aPSthSlOnXzPPR4"
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start",start))
+app.add_handler(CommandHandler("quote",quote))
+
+print("Bot is polling..")
+app.run_polling()
